6e9b02b9e918c8053281a240b2696752e30d85f0
devanshu-anand <itsdevanshu@hotmail.com>
2020-09-01 15:15:33 +0530

    ejs with includes, implemented

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(shopRoute);
 
 app.use((req,res,next) => {
     // res.status(404).sendFile(path.join(rootDir,'views','404.html'));
-    res.status(404).render('404',{pageTitle: '404 Page not found'});
+    res.status(404).render('404',{pageTitle: '404 Page not found',path: 404});
 
 });
 
